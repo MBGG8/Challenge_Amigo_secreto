b71e17661b6e08838355ed7b8e19557284bd964a
MBGG8 <micky_benji@hotmail.com>
2025-03-16 00:55:44 -0500

    Implementacion de limpieza de campo de entrada

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ function agregarAmigo() {
     alert("Por favor, inserte un nombre.");
   } else {
     amigos.push(nombre);
-    console.log(nombre);
+    console.log(amigos);
+    document.getElementById("amigo").value = "";
   }
 }
